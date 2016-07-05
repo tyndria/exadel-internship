5fce99ea62765f9bf7ffe8c88ca7e0cceaabcdd7
root <root@youhy.youhy.com>
2016-07-05 17:54:37 +0300

    Initial init

--- NodeProject/Server.js
+++ NodeProject/Server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path'); // модуль для парсинга пути
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose  = require('mongoose');
+
+
 var app = express();
 
 app.use(logger('dev')); // выводим все запросы со статусами в консоль
@@ -14,4 +17,12 @@ app.get('/api', function (req, res) {
 
 app.listen(1337, function(){
     console.log('Express server listening on port 1337');
+});
+
+app.use(cors());
+
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
