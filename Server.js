6e1d48ae50600ca76508ccad66773c43c490ff18
root <root@youhy.youhy.com>
2016-07-19 22:38:20 +0300

    Server fixes

--- Server.js
+++ Server.js
@@ -5,20 +5,13 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var mongodb = require('mongodb');
 
-var mongoose  = require('mongoose');
-
+var port = process.env.PORT || 8083;
 var mongoUri = process.env.MONGO_URL ||
   process.env.MONGOHQ_URL ||
   'mongodb://localhost:27017/austenDB';
 
- mongoose.connect(uristring, function (err, res) {
-    if (err) {
-      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-    } else {
-      console.log ('Succeeded connected to: ' + uristring);
-    }
-});
-var port = process.env.PORT || 8083;
+var mongoose  = require('mongoose');
+mongoose.connect(mongoUri);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'error connection:'));
 db.once('open', function() {
@@ -28,16 +21,16 @@ db.once('open', function() {
 
 var app = express();
 
-app.use(logger('dev')); // выводим все запросы со статусами в консоль
-app.use(bodyParser.json()); // стандартный модуль, для парсинга JSON в запросах
+app.use(logger('dev')); // выводим все запросы со статусами в консоль 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public'))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
 
 //require models 
 mongoose.model('User', require('./modules/user')); 
 mongoose.model('Test', require('./modules/test'));
 mongoose.model('Answer', require('./modules/answer')); 
-mongoose.model('Topic', require('./modules/topic'));
-mongoose.model('UsersAnswer', require('./modules/usersAnswer')); 
+mongoose.model('UserAnswer', require('./modules/userAnswer')); 
 mongoose.model('Question', require('./modules/question'));
 mongoose.model('Task', require('./modules/task'));
 
@@ -47,7 +40,7 @@ var router = require('./routes/index');
 app.use('/api', router);
 
 app.listen(port, function(){
-    console.log('Express server listening on port:'+ 8083);
+    console.log('Express server listening on port 8083');
 });
 
 app.use(cors());
@@ -57,4 +50,3 @@ app.use(function(req, res, next){
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
