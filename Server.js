61dca410bceb712003fe8811b138318eb38fc602
root <root@youhy.youhy.com>
2016-07-19 22:15:04 +0300

    Global mongo fix

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ var mongodb = require('mongodb');
 
 var mongoose  = require('mongoose');
 
-var mongoUri = process.env.MONGOLAB_URI ||
+var mongoUri = process.env.MONGO_URL ||
   process.env.MONGOHQ_URL ||
   'mongodb://localhost:27017/austenDB';
 
