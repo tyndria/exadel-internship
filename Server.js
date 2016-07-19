cde838f8e03a991c97697992c87715be7e37cf8b
root <root@youhy.youhy.com>
2016-07-19 22:03:13 +0300

    Global mongo done

--- Server.js
+++ Server.js
@@ -6,7 +6,18 @@ var cors = require('cors');
 var mongodb = require('mongodb');
 
 var mongoose  = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/austenDB');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost:27017/austenDB';
+
+ mongoose.connect(uristring, function (err, res) {
+    if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+      console.log ('Succeeded connected to: ' + uristring);
+    }
+});
 var port = process.env.PORT || 8083;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'error connection:'));
