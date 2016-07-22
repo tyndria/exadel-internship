7c8eb8d7a80756a8b97a9bff4a5ecb6267e794c7
root <root@youhy.youhy.com>
2016-07-22 13:17:29 +0300

    permissions change

--- Server.js
+++ Server.js
@@ -37,6 +37,13 @@ var message=require('./emailNotifier/notifier');
 
 //message.sendNotificationEmail({mail:'domanoffa.n@gmail.com'}, "Привет от системы");
 
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Methods", "GET","POST", "PUT");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var router = require('./routes/index');
 app.use('/api', router);
 
@@ -46,9 +53,5 @@ app.listen(port, function(){
 
 app.use(cors());
 
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 
