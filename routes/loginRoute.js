61e5f80fe9fe2a0256aa8ddf2a558d7bddbafb80
tyndria <tyndria23@gmail.com>
2016-07-28 16:17:28 +0200

    change loginRoute

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -7,7 +7,7 @@ var User = mongoose.models.User;
 var hash = require('../hashFunction');
 var authentication = require('../serverAssistance/AuthenticationAssistant');
 
-router.post('/', authorization, authentication(constants.USER_ROLE), function (req, res) {
+router.post('/', authorization, authentication([constants.USER_ROLE, constants.ADMIN_ROLE]), function (req, res) {
 	res.status(200).send(req.body);
 });
 
@@ -18,13 +18,13 @@ function authorization(req, res, next) {
 		console.log(user);
 		if (user) {
 			req.headers.authorization = token;
-			res.success = true;
-			res.auth_token = token;
+			req.body.success = true;
+			req.body.auth_token = token;
 			return next();
 		} else {
 			next(new Error("no authorization"));
 		}
-	}, next());
+	});
 }
 
 
--- serverAssistance/AuthenticationAssistant.js
+++ serverAssistance/AuthenticationAssistant.js
@@ -4,11 +4,11 @@ var User = mongoose.models.User;
 
 module.exports = (roles) => {
 	return function(req, res, next) {
-		console.log(req.body.user);
 		return User.findOne({token: req.headers.authorization})
 		.then(function(user) {
 			var isAuthorizated = false;
 			roles.forEach(function(role) {
+				console.log("user.role:" + user.role + ":" + role);
 				if (user.role.toString() == role) {
 					console.log("user.role:" + user.role + ":" + role);
 					isAuthorizated = true;
