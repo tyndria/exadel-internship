34c4b16cf9ee7664ffce4f8e6fc95fe78df0af95
root <root@youhy.youhy.com>
2016-07-28 14:33:21 +0300

    OAUS fix

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -8,7 +8,7 @@ var hash = require('../hashFunction');
 var authentication = require('../serverAssistance/AuthenticationAssistant');
 
 router.post('/', authorization, authentication(constants.USER_ROLE), function (req, res) {
-	res.status(200).send(req.body);
+	res.status(200).send(res);
 });
 
 function authorization(req, res, next) {
@@ -17,8 +17,8 @@ function authorization(req, res, next) {
 	return User.findOne({token: token.toString()}).then( function(user) {
 		console.log(user);
 		if (user) {
-			req.headers.authorization = token;
-			req.body.success = true;
+			res.headers.authorization = token;
+			res.body.success = true;
 			res.body.auth_token = token;
 			return next();
 		} else {
