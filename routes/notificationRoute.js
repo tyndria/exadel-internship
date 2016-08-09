44158b73d0337f39421ec3e0963b8619ddf758ac
tyndria <tyndria23@gmail.com>
2016-08-09 18:59:26 +0200

    fix:(

--- routes/notificationRoute.js
+++ routes/notificationRoute.js
@@ -34,14 +34,14 @@ router.get('/', authentication([constants.ADMIN_ROLE]), function(req, res) {
 				type: notification.event,
 				userId: notification.auth_id._id,
 				userName: notification.auth_id.firstName + " " + notification.auth_id.lastName,
-				_id: notification._id
+				_id: notification._id 
 			}
 			if (notification.reviewerId) {
 				newNotification.reviewerId = notification.reviewerId._id;
 				newNotification.reviewerName = notification.reviewerId.firstName + " " + notification.reviewerId.lastName;
 			}
 			notificationsToSend.push(newNotification);
-		}
+		});
 		res.send(notificationsToSend);
 	});
 });
